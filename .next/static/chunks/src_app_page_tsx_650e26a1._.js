12db60270bb0803a5b635334b379ea62cbb57a4f
mnolter <72775851+mnolter@users.noreply.github.com>
2025-08-11 15:26:35 -0300

    Add .next/static/chunks/src_app_page_tsx_650e26a1._.js 1754936795464

---
+++ .next/static/chunks/src_app_page_tsx_650e26a1._.js
@@ -0,0 +1,14 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_b8b2adf9._.js",
+  "static/chunks/node_modules_tailwind-merge_dist_bundle-mjs_mjs_b854acb4._.js",
+  "static/chunks/node_modules_lodash_90f72504._.js",
+  "static/chunks/node_modules_recharts_es6_c27a2c57._.js",
+  "static/chunks/node_modules_d3-geo_src_93ea6092._.js",
+  "static/chunks/node_modules_@radix-ui_628cacf3._.js",
+  "static/chunks/node_modules_@floating-ui_9ec1fa39._.js",
+  "static/chunks/node_modules_02cece4f._.js"
+],
+    source: "dynamic"
+});
